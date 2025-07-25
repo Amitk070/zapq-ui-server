1c479386bf835c3d1a19729651f8ed3c4db12be1
Amitk070 <kumarami663@gmail.com>
2025-07-25 21:31:52 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ import fetch from 'node-fetch';
 import uploadRouter from './upload.js';
 import { buildClaudeProjectPrompt } from './buildClaudeProjectPrompt.js';
 import { buildProjectGenPrompt } from './buildProjectGenPrompt.js';
+const { findBestFileToEdit, buildEditPrompt } = require('./utils.js');
 
 dotenv.config();
 
